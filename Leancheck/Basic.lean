03b6a8f0f15961c72e28428f4a0986f183e77f3a
Maxim <maxim.poliakov@stud.tu-darmstadt.de>
2025-05-18 23:14:11 +0200

    feat: add iterative (more comprehensible?) implementation of randIntList

--- Leancheck/Basic.lean
+++ Leancheck/Basic.lean
@@ -2,24 +2,10 @@ import Std
 
 open Std
 
-/--
-Generates a random `Int` in the interval [lo, hi] using the given `StdGen`.
-
-Returns a pair `(x, g')` where:
-- `x` is the random integer in the desired range
-- `g'` is the updated random generator
--/
 def randIntInRange (g : StdGen) (lo hi : Int) : Int × StdGen :=
   let (n, g') := randNat g (lo.toNat) (hi.toNat)
   (Int.ofNat n, g')
 
-/--
-Generates a list of random `Int`s in the interval [lo, hi] of length `len`.
-
-Returns a pair `(xs, g')` where:
-- `xs` is the list of random integers
-- `g'` is the updated random generator after generating all elements
--/
 def randIntList (g : StdGen) (len : Nat) (lo hi : Int) : List Int × StdGen :=
   match len with
   | 0 => ([], g)
@@ -28,10 +14,19 @@ def randIntList (g : StdGen) (len : Nat) (lo hi : Int) : List Int × StdGen :=
     let (xs, g2) := randIntList g1 n lo hi
     (x :: xs, g2)
 
+def randIntListIteratively (g : StdGen) (len : Nat) (lo hi : Int) : List Int × StdGen := Id.run do
+  let mut g := g
+  let mut lst := []
+  for _ in [0:len] do
+    let (x, g') := randIntInRange g lo hi
+    lst := x :: lst -- append element to beginning bc of performance
+    g := g'
+  (lst, g)
+
 def leanCheck (prop : Int → Bool) : IO Unit := do
   let mut failed : Bool := false
   let g := mkStdGen
-  let (lst, _) := randIntList g 100 0 100
+  let (lst, _) := randIntListIteratively g 100 0 100
   for x in lst do
     if ¬ prop x then
       failed := true
