f8563d0dfd96da5226fd887d5fcfded0f5e15fd9
Toni Nguyen <zero@d3vzero.de>
2025-07-09 14:54:42 +0200

    feat: Implement conditionals  (#7)
    * feat: Specify amount of tests
    
    * feat: Add parameter for conditionals
    
    * feat: Reimplement very basic conditional behaviour
    
    * feat: Implement elaborate conditional behaviour

--- Leancheck/Basic.lean
+++ Leancheck/Basic.lean
@@ -4,13 +4,21 @@ import Leancheck.Arbitrary
 
 open Std
 
+-- TODO: Update description
 /--
 Checks a given property (`prop : Int → Bool`) by running it on 100 randomly generated `Int`s
 in the interval `[0, 100]`.
 
+Parameters:
+- prop: Property to test
+- cond: Property for generated test cases
+- generator: Generator to use
+- trials: Amount of tests to run
+
 Prints:
 - A failure message for each failing input
-- A success message if all tests pass
+- A message for test cases failing the conditional
+- A success message if all tests pass and info about the conditional
 
 Usage:
 ```lean
@@ -23,6 +31,7 @@ def main : IO Unit :=
 -/
 def leanCheck {α: Type} [Arbitrary α] [ToString α]
   (prop : α → Bool)
+  (cond : α → Bool := λ x => true)
   (generator : (Option (StdGen → α × StdGen)) := none)
   (trials : Nat := 100) : IO Unit := do
 
@@ -30,13 +39,30 @@ def leanCheck {α: Type} [Arbitrary α] [ToString α]
   let mut g := mkStdGen
   let gen := generator.getD Arbitrary.generate
 
+  -- TODO Write the logic more functional
+  let mut timeout := 0
+
   for _ in [:trials] do
-    let (x, g') := gen g
+    let mut fail : Nat := 0
+    let mut (x, g') := gen g
     g := g'
+
+    while ¬ cond x do 
+      (x,g') := gen g
+      g := g'
+      fail := fail + 1
+      IO.println s!"Filter {x}"
+
+      if fail = 5 then
+        fail := 0
+        timeout := timeout + 1
+        break 
+
     if ¬ prop x then
       failed := true
       IO.println s!"Failed on {x}"
       return
 
+
   if ¬ failed then
-    IO.println "Ok, passed 100 tests."
+    IO.println s!"Ok, passed {trials - timeout} tests. {timeout} tests have timed out"
