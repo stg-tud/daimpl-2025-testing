fa538aee70ef64c601c4e59f552f5d20164350ed
Toni Nguyen <zero@d3vzero.de>
2025-07-27 20:48:48 +0200

    fix: Rework example to show failure and success

--- Leancheck/examples/Examples.lean
+++ Leancheck/examples/Examples.lean
@@ -3,4 +3,8 @@ import Std
 
 open Std
 
-#guard_msgs(error) in #eval leanCheck (λ x => x + 1 = x + 1)
+-- Example for a successful unit test
+#eval leanCheck (λ x => x + 1 = x + 1)
+
+-- Example for a failing unit test
+#eval leanCheck (λ x => x + 1 = x + 0)
