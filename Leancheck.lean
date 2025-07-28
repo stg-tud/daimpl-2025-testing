646eaaaf8b79de891a2327224a960e09b7af6156
Toni Nguyen <zero@d3vzero.de>
2025-07-28 19:49:17 +0200

    feat: Add modules to library definition

--- Leancheck.lean
+++ Leancheck.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `Leancheck` library.
 -- Import modules here that should be built as part of the library.
 import Leancheck.Basic
+import Leancheck.Arbitrary
+import Leancheck.Shrinking
