c2929180430b7b74cf7b5cb9f837b6697710ab03
MaximPLV <maxim.poliakov@stud.tu-darmstadt.de>
2025-08-19 18:34:43 +0200

    move method back

--- Leancheck/Basic.lean
+++ Leancheck/Basic.lean
@@ -50,6 +50,16 @@ partial def leanCheckCore {α: Type} [Arbitrary α] [ToString α] [Shrinking α]
     else
       leanCheckCore prop cond generatorFunc g' trials (iteration + 1)
 
+/-
+  Parse TestOutput and print human-readable version
+-/
+def parseTestOutput (x : TestOutput α) [ToString α] : IO Unit :=
+  match x with
+  | { trial := _ , iter := _ , ex := _      , shrink := _ , timeout := true } => IO.println s!"Failure: Tests have timed out. {x.iter}/{x.trial} have been tested"
+  | { trial := _ , iter := _ , ex := none   , shrink := _ , timeout := false}      => IO.println s!"Success: {x.iter}/{x.trial} passed"
+  | { trial := _ , iter := _ , ex := some a , shrink := none  , timeout := false}   => IO.println s!"Failure: Counterexample {a} found, not shrinkable"
+  | { trial := _ , iter := _ , ex := some a , shrink := some b  , timeout := false} => IO.println s!"Failure: Counterexample {a} found, shrinkable to {b}"
+
 def leanCheck {α: Type} [Arbitrary α] [ToString α] [Shrinking α]
   (prop : α → Bool)
   (cond : α → Bool := λ _ => true)
@@ -60,13 +70,3 @@ def leanCheck {α: Type} [Arbitrary α] [ToString α] [Shrinking α]
   let gen := generator.getD Arbitrary.generate
 
   parseTestOutput $ leanCheckCore prop cond gen g trials (iteration := 0) (fails := 0)
-
-/-
-  Parse TestOutput and print human-readable version
--/
-def parseTestOutput (x : TestOutput α) [ToString α] : IO Unit :=
-  match x with
-  | { trial := _ , iter := _ , ex := _      , shrink := _ , timeout := true } => IO.println s!"Failure: Tests have timed out. {x.iter}/{x.trial} have been tested"
-  | { trial := _ , iter := _ , ex := none   , shrink := _ , timeout := false}      => IO.println s!"Success: {x.iter}/{x.trial} passed"
-  | { trial := _ , iter := _ , ex := some a , shrink := none  , timeout := false}   => IO.println s!"Failure: Counterexample {a} found, not shrinkable"
-  | { trial := _ , iter := _ , ex := some a , shrink := some b  , timeout := false} => IO.println s!"Failure: Counterexample {a} found, shrinkable to {b}"
