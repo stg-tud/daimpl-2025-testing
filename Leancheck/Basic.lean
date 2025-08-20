a9573ac9b388dcfe35adfdfc8e6ab91a193ca640
MaximPLV <maxim.poliakov@stud.tu-darmstadt.de>
2025-08-20 07:28:23 +0200

    Reapply local changes after remote cleanup

--- Leancheck/Basic.lean
+++ Leancheck/Basic.lean
@@ -5,70 +5,68 @@ import Leancheck.Shrinking
 
 open Std
 
--- TODO: Update description
-/--
-Checks a given property (`prop : Int → Bool`) by running it on 100 randomly generated `Int`s
-in the interval `[0, 100]`.
+structure TestOutput (α : Type) where
+  trial    : Nat      := 0
+  iter    : Nat      := 0
+  ex      : Option α := none
+  shrink  : Option α := none
+  timeout : Bool     := false
+deriving Inhabited
 
-Parameters:
-- prop: Property to test
-- cond: Property for generated test cases
-- generator: Generator to use
-- trials: Amount of tests to run
-
-Prints:
-- A failure message for each failing input
-- A message for test cases failing the conditional
-- A success message if all tests pass and info about the conditional
-
-Usage:
-```lean
-def prop_addZero (x : Int) : Bool :=
-  x + 0 == x
-
-def main : IO Unit :=
-  leanCheck prop_addZero
-```
+-- TODO: Prove termination
+/-
+  Main method to check a property of a function
 -/
+partial def leanCheckCore {α: Type} [Arbitrary α] [ToString α] [Shrinking α]
+  (prop : α → Bool)
+  (cond : α → Bool := λ _ => true)
+  (generatorFunc : StdGen → α × StdGen)
+  (g : StdGen)
+  (trials : Nat := 100)
+  (iteration : Nat := 0)
+  (fails : Nat := 0) : TestOutput α := Id.run do
+
+  -- Check if done
+  if iteration = trials then return { trial := trials, iter := iteration }
+
+  -- Get generator and value
+  let (x, g') := generatorFunc g
+
+  -- Check conditional
+  if ¬ cond x then
+    if fails = 5 then
+      return { trial := trials, iter := iteration, timeout := true}
+    else
+      leanCheckCore prop cond generatorFunc g' (trials + 1) iteration (fails + 1)
+  else
+    -- Check property
+    if ¬ prop x then
+      let ex : TestOutput α := { trial := trials, ex := some x }
+
+      if ¬ prop (Shrinking.shrink x) then
+        return {ex with shrink := some (Shrinking.shrink x)}
+      else
+        return ex
+    else
+      leanCheckCore prop cond generatorFunc g' trials (iteration + 1)
+
+/-
+  Parse TestOutput and print human-readable version
+-/
+def parseTestOutput (x : TestOutput α) [ToString α] : IO Unit :=
+  match x with
+  | { trial := _ , iter := _ , ex := _      , shrink := _ , timeout := true } => IO.println s!"Failure: Tests have timed out. {x.iter}/{x.trial} have been tested"
+  | { trial := _ , iter := _ , ex := none   , shrink := _ , timeout := false}      => IO.println s!"Success: {x.iter}/{x.trial} passed"
+  | { trial := _ , iter := _ , ex := some a , shrink := none  , timeout := false}   => IO.println s!"Failure: Counterexample {a} found, not shrinkable"
+  | { trial := _ , iter := _ , ex := some a , shrink := some b  , timeout := false} => IO.println s!"Failure: Counterexample {a} found, shrinkable to {b}"
+
 def leanCheck {α: Type} [Arbitrary α] [ToString α] [Shrinking α]
   (prop : α → Bool)
-  (cond : α → Bool := λ x => true)
+  (cond : α → Bool := λ _ => true)
   (generator : (Option (StdGen → α × StdGen)) := none)
   (trials : Nat := 100) : IO Unit := do
 
-  let mut failed : Bool := false
-  let mut g := mkStdGen
+  let g := mkStdGen
   let gen := generator.getD Arbitrary.generate
 
-  -- TODO Write the logic more functional
-  let mut timeout := 0
-
-  for _ in [:trials] do
-    let mut fail : Nat := 0
-    let mut (x, g') := gen g
-    g := g'
-
-    while ¬ cond x do
-      (x,g') := gen g
-      g := g'
-      fail := fail + 1
-      IO.println s!"Filter {x}"
-
-      if fail = 5 then
-        fail := 0
-        timeout := timeout + 1
-        break
-
-    if ¬ prop x then
-      failed := true
-      let xShrinked := Shrinking.shrink x
-      if ¬ prop xShrinked then
-        -- IO.println s!"Failed on {x} shrinked to {xShrinked}"
-        throw (.userError s!"Failed on {x} shrinked to {xShrinked}")
-      else
-        IO.println s!"Failed on {x}"
-      return
-
-
-  if ¬ failed then
-    IO.println s!"Ok, passed {trials - timeout} tests. {timeout} tests have timed out"
+  parseTestOutput $ leanCheckCore prop cond gen g trials (iteration := 0) (fails := 0)
--- Leancheck/examples/Examples.lean
+++ Leancheck/examples/Examples.lean
@@ -1,10 +1,59 @@
+/-
+  This is a file containing some basic tests with the library
+  Either execute `lake test` for a comprehensive view or look into the source file
+-/
+
 import Leancheck
 import Std
 
 open Std
 
--- Example for a successful unit test
-#eval leanCheck (λ x => x + 1 = x + 1)
+-- Define properties or custom generators
+def prop_addZeroNat (x : Nat) : Bool :=
+  x + 0 == x
 
--- Example for a failing unit test
-#eval leanCheck (λ x => x + 1 = x + 0)
+def prop_addZeroInt (x : Int) : Bool :=
+  x + 0 == x + 1
+
+def prop_intIdempotentcy (x : Int) : Bool :=
+  x * x == x
+
+def prop_idempotentcy (x : Bool) : Bool :=
+  and x x == x
+
+def prop_float (x : Float) :=
+  x - (1.0 / (2 ^ 64)) == x
+
+def prop_listRevRev (x : List Int) :=
+  List.reverse (List.reverse x) == x
+
+def generate g :=
+    let (len, g') := randNat g 0 2
+    let rec loop (n : Nat) (acc : List Int) (g : StdGen) : List Int × StdGen :=
+      match n with
+      | 0 => (acc, g)
+      | n + 1 =>
+        let (x, g'') := Arbitrary.generate g
+        loop n (x :: acc) g''
+    loop len [] g'
+
+def prop_arrayRevRev (x : Array Int) :=
+  Array.reverse (Array.reverse x) == x
+
+def prop_revConcat (x: List Int × List Int) :=
+  let (x1, x2) := x
+  List.reverse (x1 ++ x2) == List.reverse x1 ++ List.reverse x2
+
+def main := do
+  leanCheck (λ x => x + 1 = x + 1)
+  leanCheck (λ x => x + 1 = x + 1)
+  leanCheck (λ x => x + 1 = x + 0)
+  leanCheck prop_float (λ x => x > 20) (trials := 500)
+  leanCheck prop_listRevRev
+  leanCheck prop_revConcat
+  leanCheck prop_arrayRevRev
+  leanCheck prop_listRevRev (generator := some generate)
+  leanCheck prop_addZeroInt
+  leanCheck prop_intIdempotentcy
+
+#eval main
